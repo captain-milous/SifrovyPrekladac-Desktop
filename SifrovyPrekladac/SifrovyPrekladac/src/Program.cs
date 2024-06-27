54e7dc8294268e9855735b96d94a2f9d19cfe93d
Miloš Tesař <milda.tesar3@gmail.com>
2024-06-27 23:37:18 +0200

    start of project

---
+++ SifrovyPrekladac/SifrovyPrekladac/src/Program.cs
@@ -0,0 +1,38 @@
+﻿using SifrovyPrekladac.src;
+using SifrovyPrekladac.src.forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SifrovyPrekladac
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Hlavní vstupní bod aplikace.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            bool error = false;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                ProgramInitiation.CheckStart();
+            }
+            catch 
+            {
+                Application.Run(new ConfigurationError());
+                error = true;
+            }
+            if (!error)
+            {
+                Application.Run(new MainMenuForm());
+            }
+            // Application.Run(new Form1());
+        }
+    }
+}
---
+++ SifrovyPrekladac/SifrovyPrekladac/src/ProgramInitiation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SifrovyPrekladac.src
+{
+    public static class ProgramInitiation
+    {
+        public static void CheckStart()
+        {
+            CheckStructure();
+        }
+
+        private static void CheckStructure() 
+        {
+            if (!Directory.Exists("UserFiles"))
+            {
+                Directory.CreateDirectory("UserFiles");
+            }
+            if (!Directory.Exists("UserFiles\\historie"))
+            {
+                Directory.CreateDirectory("UserFiles\\historie");
+            }
+            if (!Directory.Exists("UserFiles\\sifry"))
+            {
+                Directory.CreateDirectory("UserFiles\\sifry");
+            }
+            if (!Directory.Exists("ProgramFiles"))
+            {
+                Directory.CreateDirectory("ProgramFiles");
+            }
+            if (!Directory.Exists("ProgramFiles\\logs"))
+            {
+                Directory.CreateDirectory("ProgramFiles\\logs");
+            }
+            if (!Directory.Exists("ProgramFiles\\sifry"))
+            {
+                Directory.CreateDirectory("ProgramFiles\\sifry");
+            }
+        }
+    }
+}
